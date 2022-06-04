50c73f27137d306828fd53a2eaeb0a3a88550773
Kaveh <kavehfrounchi@gmail.com>
2022-06-04 12:48:43 +0430

    Finished MongoDB population

--- server/index.js
+++ server/index.js
@@ -1,35 +1,92 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const myObjectId = require('bson-objectid')
-const path = require('path');
-const PORT = process.env.PORT || '3001';
-const User = require('./Model/User');
+const express = require("express");
+const mongoose = require("mongoose");
+const myObjectId = require("bson-objectid");
+const path = require("path");
+const PORT = process.env.PORT || "3001";
+const User = require("./Model/User");
 const app = express();
-app.use(express.static(path.resolve(__dirname, '../client/build/')));
+app.use(express.static(path.resolve(__dirname, "../client/build/")));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/populate");
+}
 
 //get Routes
-app.get('/api', function (req, res) {
-    res.json({message: "return from server"})
+app.get("/api", function (req, res) {
+  res.json({ message: "return from server" });
 });
-app.get('*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, '../client/build/', 'index.html'));
-})
-app.post('/api/register', async function (req, res) {
-    console.log('req.body', req.body);
-    let friends = req.body.friends;
-    
-    // const user = await User.findOne({username: username}).exec();
-    // if(user) {
-    //     for(const friend of friends) {
+app.get("*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "../client/build/", "index.html"));
+});
+app.post("/api/register", async function (req, res) {
+  console.log("req.body", req.body);
+  let friends = req.body.friends;
+  let username = req.body.username;
+  const user = await User.findOne({ username: username }).exec();
+  if (!user) {
+    const userDoc = new User({
+      _id: new mongoose.Types.ObjectId(),
+      username: username,
+      friends: [],
+    });
+    for (const friend of friends) {
+      const friendUser = await User.findOne({ username: friend }).exec();
+      if (friendUser) {
+        userDoc.friends.push(friendUser._id);
+      } else {
+        const friendDoc = new User({
+          _id: new mongoose.Types.ObjectId(),
+          username: friend,
+          friends: [],
+        });
+        friendDoc.save(function (err) {
+          if (err) console.log(err);
+          return;
+        });
+        userDoc.friends.push(friendDoc._id);
+      }
+    }
+    userDoc.save(function (err) {
+      if (err) console.log(err);
+      return;
+    });
+  } else {
+    for (const friend of friends) {
+      const friendDoc = await User.findOne({ username: friend }).exec();
+      if (friendDoc) {
+        const friendIdExists = await User.findOne({
+          username: username,
+          friends: friendDoc._id,
+        });
+        if (!friendIdExists) {
+          user.friends.push(friendDoc._id);
+        }
+      } else {
+        const createFriendDoc = new User({
+          _id: new mongoose.Types.ObjectId(),
+          username: friend,
+          friends: [],
+        });
+        createFriendDoc.save(function (err) {
+          if (err) console.log(err);
+          return;
+        });
+        user.friends.push(createFriendDoc._id);
+      }
+    }
+    user.save(function (err) {
+      if (err) console.log(err);
+      return;
+    });
+  }
 
-    //     }
-    // }
-    res.json({'message' : 'Succefully registered'});
-
-})
+  res.json({ message: "Succefully registered" });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`)
-})
+  console.log(`Server listening on ${PORT}`);
+});
